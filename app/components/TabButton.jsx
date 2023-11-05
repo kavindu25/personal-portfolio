7b4e7ad705f85c36354c8a1d127df225997b30c4
kavindu25 <kavindu25@github.com>
2023-11-05 21:15:03 +0530

    feat: added about section

---
+++ app/components/TabButton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TabButton = ({ active, selectTab, children }) => {
+  const btnClasses = active
+    ? "text-white border-b border-purple-500"
+    : "text-[#ADB7BE]";
+
+  return (
+    <button onClick={selectTab}>
+      <p className={`mr-3 font-semibold hover:text-white ${btnClasses}`}>
+        {children}
+      </p>
+    </button>
+  );
+};
+
+export default TabButton;
