f1f919273927063e9ccd499f79d333538f3ff014
kavindu25 <kavindu25@github.com>
2023-11-05 10:08:49 +0530

    first commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,68 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import NavLink from "./NavLink";
+import { Bars3Icon, XmarkIcon } from "@heroicons/react/24/solid";
+import MobileMenuOverlay from "./MobileMenuOverlay";
+// import XmarkIcon from '@heroicons/react/24/outline'
+const navLinks = [
+  { title: "About", path: "#about" },
+  { title: "Projects", path: "#projects" },
+  { title: "Contact", path: "#contact" },
+];
+
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100 ">
+      <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
+        <Link
+          href="/"
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
+          Home
+        </Link>
+        <div className="mobile-menu block md:hidden">
+          {!navbarOpen ? (
+            <button onClick={() => setNavbarOpen(true)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
+              <Bars3Icon className="h-5 w-5" />
+            </button>
+          ) : (
+            <button onClick={()=> setNavbarOpen(false)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
+              {/* <XmarkIcon className="h-5 w-5"/> */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+            {navLinks.map((link, index) => {
+              return (
+                <li key={index}>
+                  <NavLink href={link.path} title={link.title} />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      {navbarOpen ? <MobileMenuOverlay links={navLinks}/> : null}
+    </nav>
+  );
+};
+
+export default Navbar;
