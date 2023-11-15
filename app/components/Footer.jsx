26ad440264d4eb03353c2358e31552f028dedeaf
kavindu25 <kavindu25@github.com>
2023-11-15 20:12:44 +0530

    fix: footer styling fixes

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer border border-[#33353F] border-l-transparent border-r-transparent text-white">
+    <footer className="footer z-10 border border-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className=" p-12 flex justify-center">
         {/* <span>Kavindu de Alwis. </span> */}
         <p className="text-slate-600"><span className="text-white px-2">Kavindu de Alwis. </span>All rights reserved 2023</p>
