d9e69e0ba288434e4466a46c03d05b09b80577d1
kavindu25 <kavindu25@github.com>
2023-11-16 11:25:24 +0530

    fix: navbar fixes

--- app/components/MobileMenuOverlay.jsx
+++ app/components/MobileMenuOverlay.jsx
@@ -3,7 +3,7 @@ import NavLink from "./NavLink";
 
 const MobileMenuOverlay = ({ links }) => {
   return (
-    <ul className="flex flex-col py-4 items-center">
+    <ul className="z-10 flex flex-col py-4 items-center">
       {links.map((link, index) => {
         return (
           <li key={index}>
